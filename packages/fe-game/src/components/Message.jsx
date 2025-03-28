f2c3ed24855bc77ad01fb6374a66f6e9bba4efc2
dmshvetsov <shvetsovdm@gmail.com>
2025-03-28 19:47:24 +0200

    fix code style

--- packages/fe-game/src/components/Message.jsx
+++ packages/fe-game/src/components/Message.jsx
@@ -1,17 +1,15 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
-import { MdVerified } from "react-icons/md";
-import { truncateText } from "../utils";
+import { MdVerified } from 'react-icons/md';
+import { truncateText } from '../utils';
 
 export default function Message({ message, isYou }) {
   const countyCode =
-    message.country && message.country !== "undefined"
-      ? message.country.toLowerCase()
-      : "";
+    message.country && message.country !== 'undefined' ? message.country.toLowerCase() : '';
   return (
-    <Box display="grid" justifyItems={isYou ? "end" : "start"}>
+    <Box display="grid" justifyItems={isYou ? 'end' : 'start'}>
       <Grid
         templateRows="30px 1fr 25px"
         templateColumns="1fr"
@@ -19,45 +17,36 @@ export default function Message({ message, isYou }) {
         px="3"
         py="2"
         borderRadius="5px"
-        borderTopLeftRadius={isYou ? "5px" : "0"}
-        borderTopRightRadius={isYou ? "0" : "5px"}
-        bg={isYou ? "#dbfff9" : "#edf3f9"}
+        borderTopLeftRadius={isYou ? '5px' : '0'}
+        borderTopRightRadius={isYou ? '0' : '5px'}
+        bg={isYou ? '#dbfff9' : '#edf3f9'}
         mt="5"
         position="relative"
         _after={{
-          position: "absolute",
+          position: 'absolute',
           content: "''",
           width: 0,
           height: 0,
-          borderStyle: "solid",
-          borderWidth: isYou ? "0px 0px 10px 10px" : "0px 10px 10px 0",
+          borderStyle: 'solid',
+          borderWidth: isYou ? '0px 0px 10px 10px' : '0px 10px 10px 0',
           borderColor: isYou
-            ? "transparent transparent transparent #dbfff9"
-            : "transparent #edf3f9 transparent transparent",
+            ? 'transparent transparent transparent #dbfff9'
+            : 'transparent #edf3f9 transparent transparent',
           top: 0,
-          left: isYou ? "auto" : "-10px",
-          right: isYou ? "-10px" : "auto",
+          left: isYou ? 'auto' : '-10px',
+          right: isYou ? '-10px' : 'auto',
         }}
       >
-        <GridItem
-          fontWeight="500"
-          fontSize="md"
-          justifySelf="start"
-          color="gray.500"
-          mb="2"
-        >
+        <GridItem fontWeight="500" fontSize="md" justifySelf="start" color="gray.500" mb="2">
           <span>{message.username} </span>
           {message.is_authenticated && (
-            <MdVerified
-              color="#1d9bf0"
-              style={{ display: "inline", marginRight: "5px" }}
-            />
+            <MdVerified color="#1d9bf0" style={{ display: 'inline', marginRight: '5px' }} />
           )}
           {countyCode && (
             <Box display="inline-block" fontSize="10px">
-              from {message.country}{" "}
+              from {message.country}{' '}
               <img
-                style={{ display: "inline-block", marginTop: "-4px" }}
+                style={{ display: 'inline-block', marginTop: '-4px' }}
                 src={`/flags/${countyCode}.png`}
                 alt={message.country}
               />
@@ -73,12 +62,7 @@ export default function Message({ message, isYou }) {
         >
           {truncateText(message.text)}
         </GridItem>
-        <GridItem
-          color="gray"
-          fontSize="10px"
-          justifySelf="end"
-          alignSelf="end"
-        >
+        <GridItem color="gray" fontSize="10px" justifySelf="end" alignSelf="end">
           {dayjs(message.timestamp).fromNow()}
         </GridItem>
       </Grid>
