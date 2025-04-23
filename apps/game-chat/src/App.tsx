f5997d10e497616da8a5549b612f1384fc72d46f
dmshvetsov <shvetsovdm@gmail.com>
2025-04-23 11:41:03 +0200

    add react router

--- apps/game-chat/src/App.tsx
+++ apps/game-chat/src/App.tsx
@@ -1,12 +1,18 @@
 import { Authenticated } from './components/Authenticated'
 import { Game } from './components/Game'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="w-screen h-screen">
-      <Authenticated component={Game} />
-    </div>
-  );
+    <BrowserRouter>
+      <div className="w-screen h-screen">
+        <Routes>
+          <Route path="/d/:slug" element={<Authenticated component={Game} />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- apps/game-chat/src/components/Game.tsx
+++ apps/game-chat/src/components/Game.tsx
@@ -1,10 +1,16 @@
 import { RealtimeChat } from "./realtime-chat";
 import { UserAccount } from "./Authenticated";
+import { Navigate, useParams } from "react-router-dom";
 
 export function Game({ userAccount }: { userAccount: UserAccount }) {
+  const { slug } = useParams<{ slug: string }>()
+  if (!slug) {
+    return <Navigate to="/" />
+  }
+
   return (
     <div className="w-[460px] h-full mx-auto px-4">
-      <RealtimeChat roomName="test" username={userAccount.username} />
+      <RealtimeChat roomName={slug} username={userAccount.username} />
     </div>
   )
 }
