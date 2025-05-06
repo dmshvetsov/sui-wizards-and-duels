012fb8a467718fb451b77476c875ba656f3e3d9e
dmshvetsov <shvetsovdm@gmail.com>
2025-05-06 16:24:26 +0200

    game-server: use realtime to communicate pairs

--- apps/game-server/supabase/functions/waitroom-pairing/index.ts
+++ apps/game-server/supabase/functions/waitroom-pairing/index.ts
@@ -3,11 +3,9 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.49.4'
 const CHANNEL = Object.freeze({ WAITROOM: 'waitroom' })
 
 interface WaitroomPresenceState {
-  [key: string]: {
-    user_id: string
-    joined_at: number
-    nonce: number
-  }[]
+  user_id: string
+  joined_at: number
+  nonce: number
 }
 
 interface WaitroomPlayer {
@@ -31,66 +29,82 @@ Deno.serve(async (_req) => {
     // Create Supabase client with the service role key for admin privileges
     const supabase = createClient(supabaseUrl, supabaseServiceKey)
 
-    const waitroom = supabase.channel(CHANNEL.WAITROOM)
+    const waitroom = supabase.channel(CHANNEL.WAITROOM, { config: { presence: { key: 'server' } } })
 
-    const presenceState = waitroom.presenceState<WaitroomPresenceState>();
-    const players: WaitroomPlayer[] = [];
-    for (const [id, presences] of Object.entries(presenceState)) {
-      if (presences.length) {
-        players.push({ id, joinedAt: presences[0].joined_at })
-        
-      }
-      
-    }
-
-
-    // Create pairs of players
-    players.sort((a, b) => a.joinedAt - b.joinedAt)
-    const pairs: [WaitroomPlayer, WaitroomPlayer][] = []
-    for (let i = 0; i < players.length - 1; i += 2) {
-      if (i + 1 < players.length) {
-        pairs.push([players[i], players[i + 1]])
-      }
-    }
-
-    // Send messages to each pair with the duel link
-    const pairingResults = await Promise.all(
-      pairs.map(async ([player1, player2]) => {
-        const duelLink = `/d/new/${player1.id}/vs/${player2.id}`
-
-        // Send a broadcast message to the waitroom channel
-        const waitRoomSendRes = await supabase.channel(CHANNEL.WAITROOM).send({
-          type: 'broadcast',
-          event: 'pairing',
-          payload: {
-            player1Id: player1.id,
-            player2Id: player2.id,
-            duelLink,
-          },
-        })
-
-        if (waitRoomSendRes === 'error') {
-          console.error('Error broadcasting pairing message')
-          return { success: false, error: 'message broadcast error' }
+    waitroom.on('presence', { event: 'join' }, ({ key, newPresences }) => {
+      console.log('joined', key, newPresences)
+    })
+    waitroom.on('presence', { event: 'sync' }, () => {
+      const presenceState = waitroom.presenceState<WaitroomPresenceState>()
+      console.log('sync waitroom presence', presenceState)
+      const players: WaitroomPlayer[] = []
+      for (const [id, presences] of Object.entries(presenceState)) {
+        const playerPresence = presences.at(0)
+        if (playerPresence) {
+          players.push({ id, joinedAt: playerPresence.joined_at })
         }
+      }
 
+      // Create pairs of players
+      players.sort((a, b) => a.joinedAt - b.joinedAt)
+      const pairs: [WaitroomPlayer, WaitroomPlayer][] = []
+      for (let i = 0; i < players.length - 1; i += 2) {
+        if (i + 1 < players.length) {
+          pairs.push([players[i], players[i + 1]])
+        }
+      }
+      console.log('presence pairs', pairs)
+
+      // TODO refactor to allSettled
+      const pairingData = pairs.map(([player1, player2]) => {
+        const duelLink = `/d/new/${player1.id}/vs/${player2.id}`
         return {
-          success: true,
-          player1Id: player1.id,
-          player2Id: player2.id,
+          user1: player1.id,
+          user2: player2.id,
           duelLink,
         }
       })
-    )
 
-    return new Response(
-      JSON.stringify({
-        success: true,
-        message: `Paired ${pairs.length} sets of players`,
-        pairs: pairingResults,
-      }),
-      { headers: { 'Content-Type': 'application/json' } }
-    )
+      // Send a broadcast message to the waitroom channel
+      supabase
+        .channel(CHANNEL.WAITROOM)
+        .send({
+          type: 'broadcast',
+          event: 'pairing',
+          payload: {
+            pairs: pairingData,
+          },
+        })
+        .then((res) => {
+          if (res === 'error') {
+            console.error('Error broadcasting pairing message')
+            return { success: false, error: 'message broadcast error' }
+          }
+
+          console.log('send success response')
+          return new Response(
+            JSON.stringify({
+              success: true,
+              message: `Paired ${pairs.length} sets of players`,
+              pairs: pairingData,
+            }),
+            { headers: { 'Content-Type': 'application/json' } }
+          )
+        })
+        // TODO disconnect from the channel
+    })
+
+    waitroom.subscribe((status) => {
+      if (status !== 'SUBSCRIBED') {
+        console.error('Failed to subscribe to waitroom channel')
+        // return new Response(JSON.stringify({ error: 'Internal server error' }), {
+        //   status: 500,
+        //   headers: { 'Content-Type': 'application/json' },
+        // })
+      }
+
+      console.log('Subscribed to waitroom channel')
+    })
   } catch (error) {
     console.error('Unexpected error:', error)
     return new Response(JSON.stringify({ error: 'Internal server error' }), {
