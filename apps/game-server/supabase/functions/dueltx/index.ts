f71aa1db2067e6cc3434793b8e3b5a0139f94404
dmshvetsov <shvetsovdm@gmail.com>
2025-05-11 23:39:48 +0200

    add back game-server
    for auto-signing feature with multisig wallet

---
+++ apps/game-server/supabase/functions/dueltx/index.ts
@@ -0,0 +1,47 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
+
+import { Ed25519Keypair, Ed25519PublicKey } from 'npm:@mysten/sui/keypairs/ed25519'
+import { MultiSigPublicKey } from 'npm:@mysten/sui/multisig'
+
+Deno.serve(async (req) => {
+  const { name, publicKey: publicKeyAddres } = await req.json()
+  const message = new TextEncoder().encode(`Hello ${name}`)
+
+  const autoSignerKeyPair = Ed25519Keypair.fromSecretKey(
+    new Uint8Array(JSON.parse(Deno.env.get('APK'))),
+    { skipValidation: true }
+  )
+  const playerPubKey = new Ed25519PublicKey(publicKeyAddres)
+
+  const multiSigPublicKey = MultiSigPublicKey.fromPublicKeys({
+    threshold: 1,
+    publicKeys: [
+      {
+        publicKey: autoSignerKeyPair.getPublicKey(),
+        weight: 1,
+      },
+      {
+        publicKey: playerPubKey,
+        weight: 1,
+      },
+    ],
+  })
+
+  const signer = multiSigPublicKey.getSigner(autoSignerKeyPair)
+  const { signature } = await signer.signPersonalMessage(message)
+
+  return new Response(
+    JSON.stringify({
+      ok: true,
+      signature,
+    }),
+    {
+      headers: { 'Content-Type': 'application/json' },
+    }
+  )
+})
