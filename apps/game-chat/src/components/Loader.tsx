73681c978b6add68495075aaa7a12470b203b6fc
dmshvetsov <shvetsovdm@gmail.com>
2025-05-23 21:34:23 +0200

    game-chat: style loader

--- apps/game-chat/src/components/Loader.tsx
+++ apps/game-chat/src/components/Loader.tsx
@@ -1,3 +1,12 @@
+import { LoaderPinwheel } from 'lucide-react'
+
 export function Loader() {
-  return <div className="flex flex-col items-center justify-center h-full">Loading...</div>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="relative inline-flex">
+        <div className="absolute transitiona-all -inset-2 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg animate-pulse"></div>
+        <LoaderPinwheel className="animate-spin" />
+      </div>
+    </div>
+  )
 }
