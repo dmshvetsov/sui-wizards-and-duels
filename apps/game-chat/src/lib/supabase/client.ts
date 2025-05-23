5fa76d7752bddbb7b33391c56b8e88a34d5bcae2
dmshvetsov <shvetsovdm@gmail.com>
2025-05-23 15:15:13 +0200

    game-chat: do not refresh supabase token
    most likely must be done manually, jwt is from zklogin not from
    supabase

--- apps/game-chat/src/lib/supabase/client.ts
+++ apps/game-chat/src/lib/supabase/client.ts
@@ -3,7 +3,13 @@ import { createClient, RealtimeChannelOptions } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-const supabaseClient = createClient(supabaseUrl, supabaseKey)
+const supabaseClient = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
 
 export const NOT_FOUND_ERR_CODE = 'PGRST116'
 
