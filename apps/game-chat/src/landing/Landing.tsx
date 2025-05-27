b46ee36360e52ba93c88e419b6c8fb676567d4c1
dmshvetsov <shvetsovdm@gmail.com>
2025-05-27 23:15:57 +0200

    landing fix path to bg

--- apps/game-chat/src/landing/Landing.tsx
+++ apps/game-chat/src/landing/Landing.tsx
@@ -17,7 +17,7 @@ function Content() {
           className="absolute h-screen w-screen my-16 z-1"
           ref={bg.ref}
           style={{
-            backgroundImage: `url('src/assets/bg.png')`,
+            backgroundImage: `url('/src/assets/bg.png')`,
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
           }}
