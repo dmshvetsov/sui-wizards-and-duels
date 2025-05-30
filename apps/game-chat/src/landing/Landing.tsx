6dd22fb7f3ae776e76282c59ea454c4ec97cf11c
dmshvetsov <shvetsovdm@gmail.com>
2025-05-30 16:45:49 +0200

    landing: fix bg style

--- apps/game-chat/src/landing/Landing.tsx
+++ apps/game-chat/src/landing/Landing.tsx
@@ -20,7 +20,6 @@ function Content() {
           backgroundImage: `url(${bgImage})`,
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
         }}
       />
 
